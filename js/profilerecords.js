ba6d7ca00dc62534b5febc288bdd0a01489f20fe
holymolyguacamolerolypolybordercollie <124778151+holymolyguacamolerolypolybordercollie@users.noreply.github.com>
2023-04-04 16:19:27 +0900

    yes
    go

--- js/profilerecords.js
+++ js/profilerecords.js
@@ -1,29 +1,34 @@
 const $tabArray = document.querySelectorAll(".profile-records__tab");
-let tabArray = Array.from($tabArray);
+const $contentArray=document.querySelectorAll(".profile-records__content");
 
 
-for(i=0;i<3;i++){
 
-    $tabArray[i].addEventListener("click",onClickTabButton);
+for(i=0;i<$tabArray.length;i++){
+    $tabArray[i].addEventListener("click",onclickTab);
 }
 
-function onClickTabButton(event){
+function onclickTab(event){
+      for(j = 0; j < $tabArray.length; j++){
+        $tabArray[j].classList.remove('active');
+        $contentArray[j].classList.remove(`active`);
+        $contentArray[j].classList.remove(`inactive`);
+    }
+    event.target.classList.add('active');
 
-
-    const clickedTabs = event.target
-    event.target.classList.remove("hidden");
-    event.target.classList.add("active");
-    
-    const restOfTabs = tabArray.splice(clickedTabs);
-
-    console.log(restOfTabs);
-
-    for(i=0;i<2;i++){
-        restOfTabs[i].classList.add("inactive");
-        restOfTabs[i].classList.remove("active");
-
+    if(event.target.classList.contains(`article`)){
+        $contentArray[0].classList.add(`active`);
+        $contentArray[1].classList.add(`inactive`);
+        $contentArray[2].classList.add(`inactive`);
+    }
+    else if(event.target.classList.contains(`comment`)){
+        $contentArray[0].classList.add(`inactive`);
+        $contentArray[1].classList.add(`active`);
+        $contentArray[2].classList.add(`inactive`);
+    }
+    else if(event.target.classList.contains(`bookmark`)){
+        $contentArray[0].classList.add(`inactive`);
+        $contentArray[1].classList.add(`inactive`);
+        $contentArray[2].classList.add(`active`);
     }
     
-
-
 }
